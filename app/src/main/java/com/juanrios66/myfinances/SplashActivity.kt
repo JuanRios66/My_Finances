3bb032560c259cf94d912df2791184579aec6877
juandas66 <judari_66@hotmail.com>
2021-04-12 15:03:46 -0500

    Splash

--- app/src/main/java/com/juanrios66/myfinances/SplashActivity.kt
+++ app/src/main/java/com/juanrios66/myfinances/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.juanrios66.myfinances
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.strictmode.IntentReceiverLeakedViolation
+import android.view.Window
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_splash)
+
+        val timer= Timer()
+        timer.schedule(timerTask { goToMainActivity() }, 2000)
+
+
+    }
+    private fun goToMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
